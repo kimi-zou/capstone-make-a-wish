708dddf842b7420393e4ecfad4a8d59214110b37
Kimi Zou <kimizou.kz@gmail.com>
2021-02-04 11:56:37 -0500

    Working on seeding Users Table

--- backend/db/seeders/20201030163017-demo-user.js
+++ backend/db/seeders/20201030163017-demo-user.js
@@ -1,32 +1,45 @@
-'use strict';
+"use strict";
 const faker = require("faker");
 const bcrypt = require("bcryptjs");
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('Users', [
+    return queryInterface.bulkInsert("Users", [
       {
-        email: 'demo@user.io',
-        username: 'Demo-lition',
-        hashedPassword: bcrypt.hashSync('password'),
+        email: "demo@user.io",
+        username: "demo",
+        displayName: "Demo",
+        hashedPassword: bcrypt.hashSync("password"),
+        birthday: new Date("1992-9-24")
+      },
+      {
+        email: "friend1@user.io",
+        username: "friend1",
+        displayName: "Demo Friend 1",
+        hashedPassword: bcrypt.hashSync("password"),
+        birthday: new Date("1993-11-23")
       },
       {
         email: faker.internet.email(),
-        username: 'FakeUser1',
+        username: "friend2",
+        displayName: "Demo Friend 2",
         hashedPassword: bcrypt.hashSync(faker.internet.password()),
+        birthday: new Date("2020-09-08")
       },
       {
         email: faker.internet.email(),
-        username: 'FakeUser2',
+        username: "friend3",
+        displayName: "Demo Friend 3",
         hashedPassword: bcrypt.hashSync(faker.internet.password()),
+        birthday: new Date("1975-05-03")
       },
     ], {});
   },
 
   down: async (queryInterface, Sequelize) => {
     const Op = Sequelize.Op;
-    return queryInterface.bulkDelete('Users', {
-      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+    return queryInterface.bulkDelete("Users", {
+      // username: { [Op.in]: ["demo", "friend1", "friend2"] }
     }, {});
   }
 };
