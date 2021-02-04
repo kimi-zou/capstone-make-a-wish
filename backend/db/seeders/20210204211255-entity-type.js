d3a7d109bfd4aebcecab53ea2c55492c188f5c13
Kimi Zou <kimizou.kz@gmail.com>
2021-02-04 16:31:11 -0500

    Add EntityTypes Table seed data

---
+++ backend/db/seeders/20210204211255-entity-type.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('EntityTypes', [
+      {
+        entity: "Friend",
+        description: "Receive a friend request.",
+      },
+      {
+        entity: "Friend",
+        description: "Friend request is accpeted.",
+      }, 
+      {
+        entity: "Birthday",
+        description: "Friend's birthday is coming up.",
+      }, 
+      {
+        entity: "Wish",
+        description: "Wish is locked.",
+      }, 
+  ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('EntityTypes', null, {});
+  }
+};
