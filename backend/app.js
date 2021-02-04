a946c823e2341a02e9c90f8d23163410c383f3ee
Kimi Zou <kimizou.kz@gmail.com>
2021-02-04 12:36:32 -0500

    Working on setting up s3

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const bodyParser = require("body-parser");
 const { ValidationError } = require('sequelize');
 
 const routes = require('./routes');
@@ -15,7 +16,8 @@ const app = express();
 app.use(morgan('dev'));
 
 app.use(cookieParser());
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Security Middleware
 if (!isProduction) {
