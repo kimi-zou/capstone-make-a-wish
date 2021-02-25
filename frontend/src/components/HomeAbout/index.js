18dd4c3c3efb41471e7aeed28b36c074a485b9c1
Kimi Zou <kimizou.kz@gmail.com>
2021-02-24 23:16:29 -0500

    Add style and content to about page

--- frontend/src/components/HomeAbout/index.js
+++ frontend/src/components/HomeAbout/index.js
@@ -8,10 +8,33 @@ const About = () => {
     <div className='about__wrapper'>
       <HomeNav />
       <div className='about__main'>
-        <div className='about__intro'>
-          <span className='about__logo'>MakeAwish </span>
-          is a birthday reminder app.
+        <div className='about__main-left'>
+          <div className='about__intro-heading'>
+            Share your birthday gift wishes on
+            <a
+              className='about__intro-heading-logo'
+              href='/'
+            > MakeAwish
+            </a>.
+          </div>
+          <div className='about__intro'>
+            MakeAwish is a birthday reminder app.
+            When friends or families' birthdays are coming up,
+            check out their wishes and send them their favourite gifts!
+          </div>
+          <div className='about__links'>
+            <a href='https://github.com/Kimi-Zou'>
+              <i className='about__icons fab fa-github' />
+            </a>
+            <a href='https://www.linkedin.com/in/kimizou'>
+              <i className='about__icons fab fa-linkedin' />
+            </a>
+            <a href='https://angel.co/u/kimi-zou'>
+              <i className='about__icons fab fa-angellist' />
+            </a>
+          </div>
         </div>
+
         <img
           className='about__background-image'
           src='https://makeawish.s3.amazonaws.com/seed-data/about-image.jpg'
